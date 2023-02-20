611c651578ea106c5294a9eeae6251c8edf008c9
OsamaAbdelhameed <osamaabdelhameed41@gmail.com>
2023-02-20 22:13:52 +0800

    heroku

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = (env, argv) => {
         entry: './src/server.ts',
         output: {
             filename: isDevMode ? 'bundle.js' : '[name].[contenthash].js',
-            path: path.resolve(__dirname, 'dist'),
+            path: path.resolve(__dirname, 'app/dist'),
             publicPath: '/'
         },
         devtool: isDevMode ? 'inline-source-map' : false,
